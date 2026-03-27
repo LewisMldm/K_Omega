1c7ddfd5390775d385e42f2e707ee37c125132cd
Lewis Massey <ldm16@leicester.ac.uk>
2026-03-27 17:56:54 +0000

    changed vent to be the controlled variable

--- Stokes/new_stokes_curtain.py
+++ Stokes/new_stokes_curtain.py
@@ -22,53 +22,51 @@ l = TestFunction(T)
 
 Re = Constant(1e5)
 Diff_coef = Constant(1)
-Produce = Constant(100)
+#Produce = Constant(100)
 
 R = FunctionSpace(M, 'R', 0)
-JetIn = Function(R).assign(500)
+JetIn = Function(R).interpolate(1)
+Vent = Function(R).interpolate(1)
 
 a = ((1/Re)*inner(grad(u), grad(v)) - inner(p, div(v)) + inner(div(u), q))*dx
-
-pol = (Diff_coef*inner(grad(t),grad(l)) + dot(u, grad(t))*l)*dx + exp(-10 * ((x - 8.5)**2 + (y - 0)**2))*l*dx 
-
-bcs = [DirichletBC(Z.sub(0), as_vector([0, JetIn*((x-7)*(x-5))]), (19,)),
-       DirichletBC(Z.sub(0), Constant((0, 0)), (21,22))]
-
-bcp = [DirichletBC(T, Produce, (22,))]
-#       DirichletBC(T, Constant(0), (19))]
-
+#bcs = [DirichletBC(Z.sub(0), as_vector([0, JetIn*((x-7)*(x-5))]), (19,)),
+#Ubdry = Function(V).interpolate(as_vector([0, JetIn*((x-7)*(x-5))]))
+Ubdry = Function(V).interpolate(as_vector([0, Vent*((-4/9)*(x-7)*(x-10))]))
+bcs = [DirichletBC(Z.sub(0), Ubdry, (23,)),
+       DirichletBC(Z.sub(0), Constant((0, 0)), (19, 21,22))]
 solve(a==0, up, bcs=bcs)
-bounds=[0.0, 10000.0]
 
-solve (pol==0, t)
+pol = (Diff_coef*inner(grad(t),grad(l)) + dot(u, grad(t))*l)*dx - 100*exp(-10 * ((x - 8.5)**2 + (y - 0)**2))*l*dx
+bcp = DirichletBC(T, Constant(0), (20))
+solve(pol==0, t, bcs=bcp)
 
-#Z1 = V * W * T
-#up1 = Function(Z1)
-#u1, p1, t1 = split(up1)
-#v1, q1, l1 = TestFunctions(Z1)
+#myfile.write(u_, p_, t)
 
-J = assemble(conditional(le(x,7), t**2, Constant(0)) * dx )
+#bcp = [DirichletBC(T, Produce, (22,))]
+#       DirichletBC(T, Constant(0), (19))]
+J = assemble(conditional(le(x,7), t**2, Constant(0)) * dx) + 0.1*assemble(Vent**2*ds(23))
+#Jhat = ReducedFunctional(J, [Control(JetIn)])
+Jhat = ReducedFunctional(J, [Control(Vent)])
+stop_annotating()
 
 u_, p_ = up.subfunctions
-
 u_.rename("Mean Velocity")
 p_.rename("Pressure")
+File = VTKFile("Stokes_opt/Vent.pvd")
+File.write(u_, p_, t)
 
-File = VTKFile("Stokes_opt/test2.pvd")
-File.write(u_, p_, t, time=1)
+print(f"Vent vel = {float(Vent):1.2f}, J = {float(Jhat(Vent)):1.2e}")
 
-Jhat = ReducedFunctional(J, [Control(JetIn)])
-
-
-stop_annotating()
 get_working_tape().progress_bar = ProgressBar
+#bounds=[0.0, 100.0]
+#optval = minimize(Jhat, options={"maxiter":10}, bounds=bounds)
+optval = minimize(Jhat)
 
-optval = minimize(Jhat, options={"maxiter":10}, bounds=bounds)
-
-JetIn.assign(optval)
-
-print(f"Inlet vel = {float(JetIn):1.2f}")
-print("final solve opt")
+# plot final pollution concetration
+JetIn.interpolate(optval)
+print(f"Vent vel = {float(Vent):1.2f}, J = {float(Jhat(Vent)):1.2e}")
+#Ubdry.interpolate(as_vector([0, JetIn*((x-7)*(x-5))]))
+Ubdry.interpolate(as_vector([0, Vent*((-4/9)*(x-7)*(x-10))]))
 solve(a==0, up, bcs=bcs)
-solve(pol==0, t)
-File.write(u_, p_, t, time=2)
+solve(pol==0, t, bcs=bcp)
+File.write(u_, p_, t)
